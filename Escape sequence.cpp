8fdb62633b0ca6a0977f91db7227abac9d510374
OmaimaSarfaraz <aimasiddique.2006@gmail.com>
2024-11-10 09:44:37 -0800

    New Files

---
+++ Escape sequence.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout<<"Hi!\tWelcome to \'Salim Habib University\'\n";
+	cout<<"How are you\?\n";
+	cout<<"Your coursess\b are:\n";
+	cout<<"\"Programming fundamentals\"\n";
+	cout<<"\"Applied Physics\"\n";
+	cout<<"\"Calculus\"\n";
+	cout<<"\"Functional English\"\n";
+	return 0;
+}
+
---
+++ iomanip.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{    
+    //Prints products with their prices
+    cout<<"PRODUCT NAME"<<setw(17)<<"PRICE PER KG"<<endl;
+    cout<<"Tomato"<<setw(15)<<135<<endl;
+    cout<<"Potato"<<setw(15)<<200<<endl;
+    cout<<"Carrot"<<setw(15)<<190<<endl;
+    cout<<"Cabbage"<<setw(14)<<100<<endl;
+   return 0;
+}
+
