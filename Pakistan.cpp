b2135903af5cdcfa3fdd5b42ce2e83f0c6f4d4a5
OmaimaSarfaraz <aimasiddique.2006@gmail.com>
2024-11-12 18:38:41 +0500

    new

---
+++ Pakistan.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+   for(int i=1;i<=5;i++)
+   {printf("%d.",i);
+	 for(int j=1;j<=i;j++)
+	  {printf("PAKISTAN ",i);
+	   }
+	   printf("\n");  
+        }
+   return 0;
+}
