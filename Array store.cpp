c5489b2357be0cb93c7222c8e548e2d8a92d9ff9
OmaimaSarfaraz <aimasiddique.2006@gmail.com>
2024-11-29 17:27:08 +0500

    2

---
+++ Array store.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{   
+	int arr[5];
+	cout<<"Enter 5 values :";
+	for(int i=0;i<5;i++)
+	{
+		cin>>arr[i];
+	}
+	for(int i=0;i<5;i++)
+	{
+		cout<<i+1<<". "<<arr[i]<<endl;
+	}
+	return 0;
+}
