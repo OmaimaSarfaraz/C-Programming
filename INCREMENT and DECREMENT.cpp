47d4d92eee79649edac45567a0de12809e229024
OmaimaSarfaraz <aimasiddique.2006@gmail.com>
2024-12-02 16:37:27 +0500

    4

---
+++ INCREMENT and DECREMENT.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int num=5; //Initialization of number
+	printf("%d \n",++num); //first add 1 in number then prints
+	printf("%d \n",num++); //first prints then add 1
+	printf("%d \n",num);   //only prints
+	printf("%d \n",++num); //first add 1 in number then prints
+	printf("%d \n",num++); //first prints then add 1
+	printf("%d \n",++num); //first add 1 in number then prints
+	return 0;
+}
+
