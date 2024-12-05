3de939999bfabe180abf96e58fd35b1e3d7f654e
OmaimaSarfaraz <aimasiddique.2006@gmail.com>
2024-12-05 07:13:13 +0500

    6

---
+++ Multiple While.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int i=10,j=1,k=20,l=5;
+	while(i>=6,j<=6,k>=16,l<=20)
+	{
+		printf("%d %d %d %d\n",i,j,k,l);
+		i--;
+		j++;
+		k=k-2;
+		l=l+5;
+	}
+	return 0;
+}
