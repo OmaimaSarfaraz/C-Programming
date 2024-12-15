1cac53dd6d8d31935239f7fb4a69ea1152145bcf
OmaimaSarfaraz <aimasiddique.2006@gmail.com>
2024-12-15 08:14:15 +0500

    +

---
+++ Mid 1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	for(int i=7;i>=1;i=i-2)
+      {
+      	printf("%d",i);
+      	   for(int j=7;j>=i;j--)
+             {
+      	        printf("PAK");
+				  }
+      	        
+              printf("\n");   } 
+	       return 0;
+	  
+	
+	
+	return 0;
+}
