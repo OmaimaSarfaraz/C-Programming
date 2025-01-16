6013f5a903389353007a4156abdd394f1f0aaaef
OmaimaSarfaraz <aimasiddique.2006@gmail.com>
2025-01-16 17:55:46 +0500

    d

---
+++ Goto loop.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int count=1;
+	loop:
+		if(count>5)
+		   goto end;
+	cout<<"Count: "<<count<<endl;
+	count++;
+    goto loop;
+	end:
+		cout<<"End of loop."<<endl;
+	
+	return 0;
+}
