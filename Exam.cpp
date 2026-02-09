c487abf8ee8a9d00664b2a0a1681d443418d5bfc
OmaimaSarfaraz <aimasiddique.2006@gmail.com>
2026-02-09 12:42:08 +0500

    PF

---
+++ Exam.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	double balance,withdraw,deposit;
+	int choice;
+	cout<<"Enter balance: ";
+	cin>>balance;
+	do
+	{
+	cout<<"\n---BANK ACCOUNT TRANSACTIONS--- \n1.Check Balance \n2.Deposit Money \n3.Withdraw Money \n4.Exit \nEnter your choice: ";
+	cin>>choice;
+	  switch(choice)
+	  {
+	    case 1: 
+	       {
+	    	    cout<<"Your Balance is: "<<balance;
+	    	    break;
+		   }
+	    case 2:
+	    	{
+	    		cout<<"How much money you want to Deposit: ";
+	    		cin>>deposit;
+	    		balance+=deposit;
+	    		if(deposit<0)
+	    		 { cout<<"Invalid! Enter positive number: ";
+	    		   continue;
+				 }
+				else
+				{ cout<<"Done! Now your balance is: "<<balance<<endl;
+				  break;
+				}
+				break;
+			}
+		case 3:
+			{
+				cout<<"How much money you want to Withdraw: ";
+	    		cin>>withdraw;
+	    		 if(withdraw<=0)
+	    		 { cout<<"Invalid! Enter positive number. ";
+	    		   continue;
+				 }
+				 if(withdraw>balance)
+				 { cout<<"Error! You have not enough balance.\nRe-enter the amount.";
+				   continue;
+				 }
+				 else
+				 { 
+				   cout<<"Money Withdrawn done"<<endl;
+				   balance-=withdraw; }
+				 break;
+			}
+	    case 4:
+	 	   {
+	 		    cout<<"Exiting the program! GoodBye.";
+	 		    break;
+		   }
+	    default:
+		   {
+			    cout<<"Invalid option! choose valid option.";
+			    continue;
+		   } 
+	  } 
+	}
+	while(choice!=4);	
+	return 0;
+}
